b054ff640f53804a7fdbbe24c31f319e3d17bbe4
Somayeh Soltanzadeh <s.solanzadeh9799@gmail.com>
2022-10-25 09:34:13 +0330

    add

--- StockTracking/frmAddStock.Designer.cs
+++ StockTracking/frmAddStock.Designer.cs
@@ -29,21 +29,21 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtProductName = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtProductPrice = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.cmbCategoryName = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.txtProductStock = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.txtProductPrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.btnClose = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
-            this.cmbCategoryName = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -51,105 +51,116 @@
             this.panel1.Controls.Add(this.dataGridView1);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(334, 0);
+            this.panel1.Location = new System.Drawing.Point(384, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(309, 584);
+            this.panel1.Size = new System.Drawing.Size(305, 582);
             this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.cmbCategoryName);
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(309, 129);
-            this.panel2.TabIndex = 0;
             // 
             // dataGridView1
             // 
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 129);
+            this.dataGridView1.Location = new System.Drawing.Point(0, 113);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(309, 455);
+            this.dataGridView1.Size = new System.Drawing.Size(305, 469);
             this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowEnter);
             // 
-            // label1
+            // panel2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(24, 28);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 20);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "CategoryName";
+            this.panel2.Controls.Add(this.cmbCategoryName);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(305, 113);
+            this.panel2.TabIndex = 0;
             // 
-            // txtProductName
+            // cmbCategoryName
             // 
-            this.txtProductName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtProductName.Location = new System.Drawing.Point(134, 60);
-            this.txtProductName.Name = "txtProductName";
-            this.txtProductName.ReadOnly = true;
-            this.txtProductName.Size = new System.Drawing.Size(184, 26);
-            this.txtProductName.TabIndex = 0;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(2, 60);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(122, 20);
-            this.label2.TabIndex = 13;
-            this.label2.Text = "Product Name";
-            // 
-            // txtProductPrice
-            // 
-            this.txtProductPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtProductPrice.Location = new System.Drawing.Point(134, 106);
-            this.txtProductPrice.Name = "txtProductPrice";
-            this.txtProductPrice.ReadOnly = true;
-            this.txtProductPrice.Size = new System.Drawing.Size(184, 26);
-            this.txtProductPrice.TabIndex = 1;
+            this.cmbCategoryName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCategoryName.FormattingEnabled = true;
+            this.cmbCategoryName.Location = new System.Drawing.Point(7, 53);
+            this.cmbCategoryName.Name = "cmbCategoryName";
+            this.cmbCategoryName.Size = new System.Drawing.Size(184, 28);
+            this.cmbCategoryName.TabIndex = 15;
+            this.cmbCategoryName.SelectedIndexChanged += new System.EventHandler(this.cmbCategoryName_SelectedIndexChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(2, 109);
+            this.label3.Location = new System.Drawing.Point(3, 19);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(116, 20);
-            this.label3.TabIndex = 15;
-            this.label3.Text = "Product Price";
+            this.label3.Size = new System.Drawing.Size(132, 20);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Category Name";
             // 
             // txtProductStock
             // 
             this.txtProductStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtProductStock.Location = new System.Drawing.Point(134, 155);
+            this.txtProductStock.Location = new System.Drawing.Point(156, 157);
             this.txtProductStock.Name = "txtProductStock";
             this.txtProductStock.Size = new System.Drawing.Size(184, 26);
-            this.txtProductStock.TabIndex = 2;
+            this.txtProductStock.TabIndex = 19;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(2, 155);
+            this.label4.Location = new System.Drawing.Point(28, 157);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(122, 20);
-            this.label4.TabIndex = 18;
+            this.label4.TabIndex = 22;
             this.label4.Text = "Product Stock";
+            // 
+            // txtProductPrice
+            // 
+            this.txtProductPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProductPrice.Location = new System.Drawing.Point(156, 113);
+            this.txtProductPrice.Name = "txtProductPrice";
+            this.txtProductPrice.ReadOnly = true;
+            this.txtProductPrice.Size = new System.Drawing.Size(184, 26);
+            this.txtProductPrice.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 20);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Product Price";
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProductName.Location = new System.Drawing.Point(156, 61);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.ReadOnly = true;
+            this.txtProductName.Size = new System.Drawing.Size(184, 26);
+            this.txtProductName.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 20);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Product Name";
             // 
             // btnClose
             // 
             this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(162, 227);
+            this.btnClose.Location = new System.Drawing.Point(196, 243);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(110, 51);
-            this.btnClose.TabIndex = 4;
+            this.btnClose.TabIndex = 24;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
@@ -157,43 +168,36 @@
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(34, 227);
+            this.btnSave.Location = new System.Drawing.Point(68, 243);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(110, 51);
-            this.btnSave.TabIndex = 3;
+            this.btnSave.TabIndex = 23;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
-            // 
-            // cmbCategoryName
-            // 
-            this.cmbCategoryName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cmbCategoryName.FormattingEnabled = true;
-            this.cmbCategoryName.Location = new System.Drawing.Point(28, 60);
-            this.cmbCategoryName.Name = "cmbCategoryName";
-            this.cmbCategoryName.Size = new System.Drawing.Size(184, 28);
-            this.cmbCategoryName.TabIndex = 0;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // frmAddStock
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(643, 584);
+            this.ClientSize = new System.Drawing.Size(689, 582);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.txtProductStock);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txtProductPrice);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtProductName);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
             this.Name = "frmAddStock";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Add Stock";
+            this.Load += new System.EventHandler(this.frmAddStock_Load);
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -204,15 +208,15 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtProductName;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtProductPrice;
+        private System.Windows.Forms.ComboBox cmbCategoryName;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtProductStock;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtProductPrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.ComboBox cmbCategoryName;
     }
 }
--- StockTracking/frmAddStock.cs
+++ StockTracking/frmAddStock.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using StockTracking.BLL;
+using StockTracking.DAL.DTO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,6 +22,76 @@ namespace StockTracking
         private void btnClose_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+        ProductBLL bll = new ProductBLL();
+        ProductDTO dto = new ProductDTO();
+        private void frmAddStock_Load(object sender, EventArgs e)
+        {
+            dto = bll.Select();
+            dataGridView1.DataSource=dto.Products;
+            dataGridView1.Columns[0].HeaderText = "Product Name";
+            dataGridView1.Columns[1].HeaderText = "Category Name";
+            dataGridView1.Columns[2].HeaderText = "Stock Amount";
+            dataGridView1.Columns[3].HeaderText = "Price";
+            dataGridView1.Columns[4].Visible = false;
+            dataGridView1.Columns[5].Visible = false;
+
+            cmbCategoryName.DataSource = dto.Categories;
+            cmbCategoryName.DisplayMember = "CategoryName";
+            cmbCategoryName.ValueMember = "ID";
+            cmbCategoryName.SelectedIndex = -1;
+            if (dto.Categories.Count > 0)
+                combofull = true;
+
+        }
+        bool combofull = false;
+        private void cmbCategoryName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (combofull)
+            {
+                List<ProductDetailDTO> list = dto.Products;
+                list = list.Where(x => x.CategoryID == Convert.ToInt32(cmbCategoryName.SelectedValue)).ToList();
+                dataGridView1.DataSource = list;
+                if(list.Count ==0)
+                {
+                    txtProductPrice.Clear();
+                    txtProductName.Clear();
+                    txtProductStock.Clear();    
+                }
+            }
+        }
+        ProductDetailDTO detail = new ProductDetailDTO();
+        private void dataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            detail.ProductName = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtProductName.Text = detail.ProductName;
+            detail.Price = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[3].Value);
+            txtProductPrice.Text= detail.Price.ToString();
+            detail.StockAmount = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[2].Value);
+            detail.ProductID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[4].Value);
+
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(txtProductName.Text.Trim()=="")
+                MessageBox.Show("Please select a product From Table");
+            else if (txtProductStock.Text.Trim()=="")
+                MessageBox.Show("Please give a stock Amount");
+            else
+            {
+                int sumstock = detail.StockAmount;
+                sumstock += Convert.ToInt32(txtProductStock.Text);
+                detail.StockAmount = sumstock;
+                if(bll.Update(detail))
+                {
+                    MessageBox.Show("Stock was Added");
+                    dto = bll.Select();
+                    dataGridView1.DataSource = dto.Products;
+                    txtProductStock.Clear();
+                }
+            }
         }
     }
 }
